1d2f2ee713ce950f6bfc71a4f89f4c0e5f6ee720
furkany2968-stack <furkany.2968@gmail.com>
2026-05-01 16:27:29 +0300

    Update CardData.cs
    enum ekledim ve açıklama satırı koydum

--- Assets/_Game/Cards/CardData.cs
+++ Assets/_Game/Cards/CardData.cs
@@ -6,24 +6,24 @@ public class CardData : ScriptableObject
     
     [Header("Temel Bilgiler")]
     public string cardName;
-    public string cardIrk;
-    public string cardTip;
-    public string cardNadirlik;
+    public enum cardIrk;
+    public enum cardTip;
+    public enum cardNadirlik;
     
     [Header("İstatistikler")]
-    public int cardDeger;
+    public int cardDeger; //Kart maliyeti. Kartı çağırmak için gereken enerji
     public int cardCan;
-    public bool cardZirh;
+    public bool cardZirh; //Ek can ya da ön can. İlk darbeyi karşılar. 0 olup kırılsa bile o saldırı ana cana vuramaz
     public int cardHasar;
     public float cardHiz;
-    public int cardDefans;
+    public int cardDefans; //Yediği bütün saldırıları azaltır. 120 hasar - 30 defans = 90 can gider.
     
     [Header("Özellikler")]
-    public bool cardToplama;
+    public bool cardToplama; //Kart oyundayken birkaç turda bir enerji kazandırır
     public int cardToplamaSuresi;
-    public bool cardCalma;
-    public bool cardGizlenme;
-    public bool cardTespit;
+    public bool cardCalma; //Son darbeyi vurduğu rakip kartın değerinin bir kısmını kazandırır
+    public bool cardGizlenme; //İlk darbeyi vurmadan önce görünmez
+    public bool cardTespit; //Gizlenmeyi iptal eder
     public bool cardYakma;
 
 }
